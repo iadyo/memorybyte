8dea3ebfb6d1302a65926b3e37e977ea61e91ec9
iadyo <iamadyoo@gmail.com>
2025-03-25 00:04:53 +0100

    jd

--- apps/frontend/src/layouts/FooterLayout.tsx
+++ apps/frontend/src/layouts/FooterLayout.tsx
@@ -1,16 +1,27 @@
-import { FaDiscord, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
+import {
+  FaDiscord,
+  FaGithub,
+  FaInstagram,
+  FaYoutube,
+  FaMoon,
+  FaSun,
+} from "react-icons/fa";
 import { useNavigate } from "react-router";
+import useDarkMode from "../utils";
 
 export default function Footer() {
+  const [darkMode, toggleDarkMode] = useDarkMode();
   const navigate = useNavigate();
 
   return (
     <footer className="flex flex-wrap bg-white dark:bg-gray-700 dark:text-white p-13">
       <div className="w-full lg:w-1/2">
         <h2 className="font-bold text-xl">MemoryByte</h2>
-        <p>&copy;2025 MemoryByte</p>
+        <p className="text-gray-600 dark:text-gray-300 pt-3">
+          Ucz się z fiszek, testów i innych aktywności.
+        </p>
 
-        <div className="flex space-x-3 mt-3">
+        <div className="flex space-x-3 mt-3 text-xl">
           <a href="#" target="_blank" rel="noreferrer">
             <FaYoutube />
           </a>
@@ -24,28 +35,44 @@ export default function Footer() {
             <FaGithub />
           </a>
         </div>
-
-        <hr className="w-full border-t border-gray-200 dark:border-gray-600 my-5" />
       </div>
 
-      <div className="w-full lg:w-1/2 flex flex-wrap lg:flex-nowrap justify-evenly lg:text-right">
-        <div className="w-full lg:w-1/4 space-y-3">
-          <h2 className="font-bold text-2xl">Nawigacja</h2>
-          <ul className="text-gray-600 dark:text-gray-300 cursor-pointer">
-            <li onClick={() => navigate("#")}>Lorem ipsum1</li>
-            <li onClick={() => navigate("#")}>Lorem ipsum2</li>
-            <li onClick={() => navigate("#")}>Lorem ipsum3</li>
-            <li onClick={() => navigate("#")}>Lorem ipsum4</li>
+      <div className="w-full lg:w-1/2 flex flex-wrap lg:flex-nowrap justify-evenly text-sm pt-10 lg:pt-0">
+        <div className="w-full lg:w-1/3 space-y-3">
+          <h2 className="font-semibold uppercase tracking-wider">Nawigacja</h2>
+          <ul className="text-gray-600 dark:text-gray-300 cursor-pointer space-y-2">
+            <li onClick={() => navigate("#")}>Strona główna</li>
+            <li onClick={() => navigate("#")}>Fiszki</li>
+            <li onClick={() => navigate("#")}>Testy</li>
           </ul>
         </div>
-        <div className="w-full lg:w-1/4 space-y-3 mt-5 lg:mt-0">
-          <h2 className="font-bold text-2xl">Inne gówna</h2>
-          <ul className="text-gray-600 dark:text-gray-300 cursor-pointer">
-            <li onClick={() => navigate("#")}>Lorem ipsum1</li>
-            <li onClick={() => navigate("#")}>Lorem ipsum2</li>
-            <li onClick={() => navigate("#")}>Lorem ipsum3</li>
+        <div className="w-full lg:w-1/3 space-y-3 mt-5 lg:mt-0">
+          <h2 className="font-semibold uppercase tracking-wider">Informacje</h2>
+          <ul className="text-gray-600 dark:text-gray-300 cursor-pointer space-y-2">
+            <li onClick={() => navigate("#")}>Polityka prywatności</li>
+            <li onClick={() => navigate("#")}>Kontakt</li>
           </ul>
         </div>
+        <div className="w-full lg:w-1/3 space-y-3 mt-5 lg:mt-0">
+          <h2 className="font-semibold uppercase tracking-wider">Konto</h2>
+          <ul className="text-gray-600 dark:text-gray-300 cursor-pointer space-y-2">
+            <li onClick={() => navigate("#")}>Logowanie</li>
+            <li onClick={() => navigate("#")}>Rejestracja</li>
+          </ul>
+        </div>
+      </div>
+      <hr className="w-full border-t border-gray-200 dark:border-gray-600 my-10" />
+      <div className="w-full flex-col lg:flex-row flex items-center lg:justify-between dark:text-gray-300 text-gray-600">
+        <span className="text-sm">
+          &copy; 2025 MemoryByte. Wszelkie prawa zastrzeżone.
+        </span>
+        <button onClick={() => toggleDarkMode((prev) => !prev)}>
+          {darkMode ? (
+            <FaSun className="cursor-pointer" />
+          ) : (
+            <FaMoon className="cursor-pointer" />
+          )}
+        </button>
       </div>
     </footer>
   );
--- apps/frontend/src/layouts/HeaderLayout.tsx
+++ apps/frontend/src/layouts/HeaderLayout.tsx
@@ -3,12 +3,9 @@ import Button from "../components/Button";
 import Input from "../components/Input";
 import { MainLabel } from "../components/Label";
 import { FiPlus, FiSearch } from "react-icons/fi";
-import { FaMoon, FaSun } from "react-icons/fa";
-import useDarkMode from "../utils";
 
 export default function Header() {
   const navigate = useNavigate();
-  const [darkMode, toggleDarkMode] = useDarkMode();
 
   return (
     <header className="flex items-center justify-between">
@@ -31,13 +28,6 @@ export default function Header() {
       </div>
 
       <nav className="flex items-center gap-7">
-        <Button onClick={() => toggleDarkMode((prev) => !prev)}>
-          {darkMode ? (
-            <FaSun className="text-xl cursor-pointer" />
-          ) : (
-            <FaMoon className="text-xl cursor-pointer" />
-          )}
-        </Button>
         <Button onClick={() => navigate("/create")}>
           Stwórz <FiPlus className="text-xl inline" />
         </Button>
