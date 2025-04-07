f5ebb1d1f6e26c02f490387535557bea9d3990ec
verifiedfemboy <maciekplaygamesyt@gmail.com>
2025-04-07 13:28:31 +0200

    flipcard create

--- apps/backend/src/models/flipcard.rs
+++ apps/backend/src/models/flipcard.rs
@@ -1,4 +1,5 @@
 use serde::Serialize;
+use sqlx::MySqlPool;
 
 #[derive(Serialize)]
 pub struct FlipCard {
@@ -6,4 +7,28 @@ pub struct FlipCard {
     pub first_side: String,
     pub second_side: String,
     pub category_id: i32,
+}
+
+impl FlipCard {
+    
+    pub async fn create_flipcard(
+        pool: &MySqlPool, 
+        first_side: &str, 
+        second_side: &str, 
+        category_id: i32
+    ) -> Result<Self, sqlx::Error> {
+        let query = sqlx::query("INSERT INTO flipcards (first_side, second_side, category_id) VALUES (?, ?, ?)")
+            .bind(first_side)
+            .bind(second_side)
+            .bind(category_id)
+            .execute(pool)
+            .await?;
+
+        Ok(Self {
+            id: query.last_insert_id() as i32,
+            first_side: first_side.to_string(),
+            second_side: second_side.to_string(),
+            category_id,
+        })
+    }
 }
